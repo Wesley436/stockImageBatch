202ccf30c0c8671e8b4baa220c20afcf8e3f96b5
WesleyHui <wesleyhuichunwai@gmail.com>
2022-10-19 17:25:38 +0800

    file upload, clear file, using axios to send forma data with the txt file, job details displays

--- src/components/StockImageBatch.jsx
+++ src/components/StockImageBatch.jsx
@@ -1,32 +1,103 @@
 import { Button, Grid } from '@mui/material';
 import { TextField, Typography } from '@material-ui/core';
+import Link from '@mui/material/Link';
 import '../styles/App.css';
 import { useContext } from 'react';
 import { StockImageContext } from '../context/StockImageBatchContextProvider';
 
 export default function StockImageBatch() {
     const {
+        setInputFile,
         inputPrompt, setInputPrompt,
         inputPrompts, setInputPrompts,
+        sendBatchJob,
+        jobFile,
+        requestedAt,
+        startedAt,
+        finishedAt,
+        resultUrl,
+        remarks,
+        resultImageLinks,
     } = useContext(StockImageContext);
+
+    const readFile = e => {
+        const file = e.target.files[0];
+        setInputFile(file);
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => {
+            const inputFileLines = reader.result.replace(/\t/g, '').replace(/\r/g, '').split('\n').filter(function(i){return i})
+            setInputPrompts(inputFileLines);
+        }
+    }
+
     return(
         <Grid container>
             <Grid item lg={6} sx={{padding:'1rem'}}>
-                <TextField varient='outlined' value={inputPrompt} onChange={(e)=>{setInputPrompt(e.target.value)}}/>
-                <Button disabled={!inputPrompt} variant='contained' onClick={()=>{setInputPrompts([...inputPrompts, inputPrompt]);setInputPrompt('');}}>Add</Button>
+                {/* <TextField varient='outlined' value={inputPrompt} onChange={(e)=>{setInputPrompt(e.target.value)}}/>
+                <Button disabled={!inputPrompt} variant='contained' onClick={()=>{setInputPrompts([...inputPrompts, inputPrompt]);setInputPrompt('');}}>Add</Button> */}
+                <Button variant='contained' component='label'>
+                    File
+                    <input type='file' accept='.txt' hidden onChange={readFile}/>
+                </Button>
+                <Button disabled={!inputPrompts.length} variant='contained' onClick={() => {setInputFile();setInputPrompts([]);}}>
+                    Clear file
+                </Button>
+                <Button disabled={!inputPrompts.length} variant='contained' onClick={sendBatchJob}>
+                    Send
+                </Button>
                 {inputPrompts.map((prompt, i) => {
                     return(
-                    <Typography key={i} align='left'>
-                        {prompt}
-                    </Typography>
+                        <Typography key={i} align='left'>
+                            {prompt}
+                        </Typography>
                     )
                 })}
-                <div>
-                    <Button disabled={!inputPrompts.length} variant='contained' onClick={()=>{setInputPrompts([])}}>Send</Button>
-                </div>
             </Grid>
             <Grid item lg={6}>
-
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        job file:
+                        <Link href={jobFile}>
+                            {jobFile}
+                        </Link>
+                    </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        {'request at:' + requestedAt}
+                    </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        {'started at:' + startedAt}
+                    </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        {'finished at:' + finishedAt}
+                    </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        {'result url:' + resultUrl}
+                    </Typography>
+                </Grid>
+                <Grid item lg={12}>
+                    <Typography align='left'>
+                        {'remarks:' + remarks}
+                    </Typography>
+                </Grid>
+                {/* {resultImageLinks.map((resultImageLink, i) => {
+                    return(
+                        <Grid key={i} item lg={6}>
+                            <img alt={resultImageLink?'Output':null} src={resultImageLink} style={{alignSelf: 'center', maxWidth: '512px', maxHeight: '512px', margin: '1rem'}}/>
+                            <Typography align='center'>
+                                {inputPrompts[i]}
+                            </Typography>
+                        </Grid>
+                    )
+                })} */}
             </Grid>
         </Grid>
     )
--- src/context/StockImageBatchContextProvider.jsx
+++ src/context/StockImageBatchContextProvider.jsx
@@ -1,16 +1,110 @@
 import { createContext, useState } from 'react';
+import axios from 'axios';
 
 export const StockImageContext = createContext();
 
 export default function StockImageContextProvider(props) {
+    const [inputFile, setInputFile] = useState();
     const [inputPrompt, setInputPrompt] = useState('');
     const [inputPrompts, setInputPrompts] = useState([]);
+    const [jobId, setJobId] = useState('');
+    const [jobFile, setJobFile] = useState('');
+    const [requestedAt, setRequestedAt] = useState('');
+    const [startedAt, setStartedAt] = useState('');
+    const [finishedAt, setFinishedAt] = useState('');
+    const [resultUrl, setResultUrl] = useState('');
+    const [remarks, setRemarks] = useState('');
+    const [resultImageLinks, setResultImageLinks] = useState([]);
+
+    const batchRequestURL = 'https://TEMP.execute-api.us-east-1.amazonaws.com';
+
+    let batchingJobStatus;
+    function checkJobStatus(jobId) {
+        let formData = new FormData();
+        formData.append('token', 'token');
+        formData.append('job_id', jobId);
+        axios.post(
+            batchRequestURL+'/fetchJob',
+            formData,
+            {
+                headers:{
+                    "Authorization": "authorization",
+                    "Content-type": "multipart/form-data",
+                },
+            }
+        )
+        .then(response => {
+            return response.data;
+        })
+        .then(json => {
+            let response = json.response;
+            if (json.success){
+                if(response.job_status === 'finished'){
+                    clearInterval(batchingJobStatus);
+                    clearTimeout(requestTimeout);
+                    setJobFile(response.job_file);
+                    setResultUrl(response.result_url);
+                }else if (response.job_status === 'pending'){
+
+                }
+            }else {
+                console.log(response.message);
+                clearInterval(batchingJobStatus);
+                clearTimeout(requestTimeout);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    let requestTimeout;
+    function sendBatchJob(){
+        let formData = new FormData();
+        formData.append('token', 'token');
+        formData.append('job_file', inputFile);
+        axios.post(
+            batchRequestURL+'/requestJob',
+            formData,
+            {
+                headers:{
+                    "Authorization": "authorization",
+                    "Content-type": "multipart/form-data",
+                },
+            }
+        )
+        .then(response => {
+            return response.data;
+        })
+        .then(json => {
+            let response = json.response
+            if(json.success){
+                setJobId(response.job_id);
+                batchingJobStatus = setInterval(function () {checkJobStatus(response.job_id)}, 1000);
+            }else{
+                console.log(response.message);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
 
     return (
         <StockImageContext.Provider
             value={{
+                inputFile, setInputFile,
                 inputPrompt, setInputPrompt,
                 inputPrompts, setInputPrompts,
+                sendBatchJob,
+                jobId, setJobId,
+                jobFile, setJobFile,
+                requestedAt, setRequestedAt,
+                startedAt, setStartedAt,
+                finishedAt, setFinishedAt,
+                resultUrl, setResultUrl,
+                remarks, setRemarks,
+                resultImageLinks, setResultImageLinks,
             }}
         >
             {props.children}
